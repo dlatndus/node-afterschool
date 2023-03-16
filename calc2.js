4aada3b09ff99099b44d98df74335c3bef9a305b
Sooyeon Lim <s2129@e-mirim.hs.kr>
2023-03-16 19:17:19 +0900

    export 연습

---
+++ calc2.js
@@ -0,0 +1,10 @@
+//2023.03.16
+//계산기 
+//더하기 함수가 메인 자바스크립트 파일과 분리한 경우
+
+var calc = {};
+calc.add = function(a, b){
+    return a+b;
+}
+
+module.exports = calc;
--- ch02-test05.js
+++ ch02-test05.js
@@ -3,5 +3,10 @@
 //더하기 함수를 모듈로 분리한 calc.js 파일을 불러들임
 //exports 가 불러들인 객체
 
+//exports 사용
 var calc = require('./calc');
 console.log(`모듈 분리후 - calc.add 함수 호출 결과 ${calc.add(10, 10)}`)
+
+//module.exports 사용
+var calc2 = require('./calc2');
+console.log(`모듈 분리후 - calc.add2 함수 호출 결과 ${calc2.add(10, 10)}`)

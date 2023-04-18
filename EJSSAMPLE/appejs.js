1593bf2d3bb7d18f0bfd5366be58ad95c02c2ef7
Sooyeon Lim <96305365+printyeon@users.noreply.github.com>
2023-04-18 19:59:25 +0900

    ejs 사용해서 파일 읽어오기

---
+++ EJSSAMPLE/appejs.js
@@ -0,0 +1,71 @@
+/*<%code%> js 코드를 입력
+    <%=value%> 데이터를 출력 */
+
+let express = require('express');
+let fs = require('fs');
+let ejs = require('ejs');
+let app = express();
+
+//express 모듈
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+
+app.get('/', (req, res)=>{
+    console.log('/get start ejs test...');
+    fs.readFile('ejsex02.ejs', "utf-8", (err, data) =>{
+        if(err)
+        {
+            console.log('파일 읽기 에러' + err);
+        }
+        else{
+            res.send(ejs.render(data));
+        }
+    })
+});
+
+app.get('/login', (request, response)=>{
+    console.log('/login get...');
+    fs.readFile('login.ejs', "utf-8", (err, data) =>{
+        if(err)
+        {
+            console.log('login.ejs 파일 읽기 에러' + err);
+        }
+        else{
+            response.send(ejs.render(data));
+        }
+})
+app.post('/login',(req,res) =>{
+    let inId = req.body.id;
+    let inPass = req.body.password;
+    console.log('입력한 값' + inId + inPass);
+    if(inId == 'admin' && inPass=='1234'){
+        res.redirect('/');
+    }
+    else{
+        console.log('로그인 실패' + inId + inPass);
+        res.redirect('/login');
+    }
+})
+
+});
+
+
+app.listen(3000, ()=>{
+    console.log("server starting 3000");
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
